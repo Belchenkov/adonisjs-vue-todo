b656adfc62402cdff47cba4809b74de98e6f4b43
Алексей Бельченков <u608110@gmail.com>
2019-04-29 11:44:07 +0400

    Register

--- server/start/routes.js
+++ server/start/routes.js
@@ -1,21 +1,10 @@
-'use strict'
-
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URLs and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.0/routing
-|
-*/
+'use strict';
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route')
+const Route = use('Route');
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
+Route.group(() => {
+    Route.post('auth/register', 'UserController.register')
 })
+    .prefix('api/v0');
+
